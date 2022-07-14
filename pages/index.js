775859a69d21d054a553eb1ac09e1bc2f2b767c2
jeevakalaiselvam <jeevak001@outlook.com>
2022-07-14 17:17:04 +0530

    ADD UseState Reference

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return <div class={styles.container}></div>;
+  const [value, setValue] = useState('INITIAL VALUE');
+
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      new Promise((resolve, reject) => {
+        if (true) {
+          setTimeout(() => {
+            resolve('UPDATED VALUE');
+          }, 3000);
+        }
+      })
+        .then((data) => {
+          setValue((old) => data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    getData();
+  }, []);
+
+  return (
+    <div className="container">
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setValue((old) => 'UPDATED VALUE');
+        }}
+      >
+        UPDATE
+      </button>
+      <h1 class="mt-4 text-secondary">{value}</h1>
+    </div>
+  );
 }
